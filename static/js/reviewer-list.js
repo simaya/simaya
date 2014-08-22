7bfb7c54e94d2db661e9e48ff76853387912bb51
Mohammad Anwari <mdamt@mdamt.net>
2014-08-22 09:05:35 +0700

    Reviewer list

---
+++ static/js/reviewer-list.js
@@ -0,0 +1,40 @@
+var updateReviewerList = function() {
+  var populateReviewerList = function(data) {
+    var $list = $("#reviewers-list");
+    $list.children(":not(.template)").remove();
+    for (var i = 0; i < data.length; i ++) {
+      var item = data[i];
+      var $item = $list.find(".template").clone();
+      $item.removeClass("hidden");
+      $item.removeClass("template");
+      $item.find(".step").text(item.profile.fullName);
+      $item.find(".title").text(item.profile.title);
+      $list.append($item);
+    }
+  }
+
+  var sender = $("#sender").val();
+  var letterId = $("[name=id]").val();
+  $("#reviewers-loading").removeClass("hidden");
+  $.ajax({
+    url: "/letter/reviewers-by-user/" + sender + "/" + letterId, 
+    dataType: "json"
+  }).always(function () {
+    $("#reviewers-loading").addClass("hidden");
+  }).done(function (result) {
+    populateReviewerList(result);
+
+  });
+
+
+}
+
+$(document).ready(function() {
+  // Update reviewer list
+  $("#sender").change(function() {
+    updateReviewerList();
+  });
+
+  updateReviewerList();
+
+});
