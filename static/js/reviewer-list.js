a828e461fb7e24a290de6926707e665ddb16536d
Mohammad Anwari <mdamt@mdamt.net>
2014-08-23 22:31:23 +0700

    Review status

--- static/js/reviewer-list.js
+++ static/js/reviewer-list.js
@@ -1,14 +1,51 @@
+// Dependency: moment, resolve-name
 var updateReviewerList = function() {
+  moment.lang("id");
   var populateReviewerList = function(data) {
     var $list = $("#reviewers-list");
+
+    var originator = $list.attr("data-originator");
+    var currentReviewer = $list.attr("data-current");
+    var date = $list.attr("data-date");
+    data.unshift({
+      date: date,
+      username: originator,
+      profile: {
+        fullName: originator
+      },
+      needResolve: true
+    });
+    data.push({
+      username: "",
+      profile: {
+        fullName: "Tata Usaha"
+      }
+    });
     $list.children(":not(.template)").remove();
+    var width = (100/data.length);
     for (var i = 0; i < data.length; i ++) {
       var item = data[i];
       var $item = $list.find(".template").clone();
       $item.removeClass("hidden");
       $item.removeClass("template");
-      if (item.current) $item.addClass("active");
-      $item.find(".step").text(item.profile.fullName);
+      if (item.action == "approved") 
+        $item.addClass("review-approved");
+      else if (item.action == "declined") 
+        $item.addClass("review-declined");
+      if (item.username == currentReviewer) 
+        $item.addClass("review-current");
+      $item.css("width", width + "%");
+      var step = $item.find(".step");
+      step.text(item.profile.fullName);
+      if (item.needResolve) {
+        step.resolveUserNames();
+      }
+      if (item.date) {
+        var tooltip = moment(item.date).fromNow();
+        if (item.message) 
+          tooltip += ": " + item.message;
+        step.tooltip({placement: "bottom", title: tooltip});
+      }
       $item.find(".title").text(item.profile.title);
       $list.append($item);
     }
