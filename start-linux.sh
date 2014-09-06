1db16bff713a01f498fb6ff31f324f2899c6e638
Nurul Arif Setiawan <n.arif.setiawan@gmail.com>
2014-09-06 14:59:35 +0000

    change mongodb server

--- start-linux.sh
+++ start-linux.sh
@@ -3,7 +3,6 @@
 APPPATH=~/apps/simaya
 ln -s ~/apps/simaya/ownbox/models ~/apps/simaya/ob
 ln -s ~/apps/simaya/sinergis-base/sinergis ~/apps/simaya/sinergis
-export DB=simaya
 
 # Get index of simaya app in forever process list
 if [ $(forever list | grep simaya/app.js | awk '{print $2}' | cut -c2) ]
@@ -13,5 +12,7 @@ then
 else
     echo "Simaya is not running"
     export PORT=3000
+    export DBHOST=10.0.1.5
+    export DB=simaya
     forever start ~/apps/simaya/app.js > /dev/null
 fi
