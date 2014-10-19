91868be20c7eb0fd320a7e1f187ef06bb825b81f
Mohammad Anwari <mdamt@mdamt.net>
2014-10-20 06:45:52 +0700

    Migrate old data

---
+++ tools/migrate-simaya-l.js
@@ -0,0 +1,30 @@
+// This scripts gives modifiedDate field for simaya L 
+var settings = require('../settings.js')
+
+var app = {
+  db: function(modelName) {
+    return settings.model(settings.db, modelName);
+  }
+  , ObjectID: settings.ObjectID
+  , validator: settings.validator
+}
+
+var set = function(name, cb) {
+  var date = new Date(0);
+  var db = app.db(name);
+  db.update({}, {
+    $set: { modifiedDate: date }
+  }, { multi: true }, cb)
+}
+
+console.log("Standing by...");
+settings.db.open(function(){
+  set("letter", function(err, result) {
+    if (err) console.log(err);
+    set("user", function(err, result) {
+      if (err) console.log(err);
+      process.exit();
+    });
+  });
+
+});
