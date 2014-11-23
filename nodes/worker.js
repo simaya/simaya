edb641fe244b64b5269539a244da598a4cbb71b9
Mohammad Anwari <mdamt@mdamt.net>
2014-11-23 13:53:20 +0700

    Move sync functions to worker

--- nodes/worker.js
+++ nodes/worker.js
@@ -28,5 +28,68 @@ worker.addFunction("moveOrganization", function(job) {
 
 });
 
+worker.addFunction("finalize", function(job) {
+  if (job.payload && job.payload.length > 0) {
+    var payload = JSON.parse(job.payload.toString());
+    if (payload.syncId) {
+      var options = {
+        syncId: payload.syncId 
+      }
+      node.finalizeSync(options, function(err, result) {
+        if (err) {
+          job.workComplete(JSON.stringify({result: false, reason: err.message}));
+        } else {
+          job.workComplete(JSON.stringify({result: true, data: "ok"}));
+        }
+      });
+      return;
+    }
+  }
+  job.workComplete(JSON.stringify({result: false, reason: "no payload"}));
+
+});
+
+worker.addFunction("request", function(job) {
+  if (job.payload && job.payload.length > 0) {
+    var payload = JSON.parse(job.payload.toString());
+    if (payload.installationId) {
+      var options = {
+        installationId: payload.installationId 
+      }
+      node.requestSync(options, function(err, result) {
+        if (err) {
+          job.workComplete(JSON.stringify({result: false, reason: err.message}));
+        } else {
+          job.workComplete(JSON.stringify({result: true, data: "ok"}));
+        }
+      });
+      return;
+    }
+  }
+  job.workComplete(JSON.stringify({result: false, reason: "no payload"}));
+
+});
+
+worker.addFunction("prepare", function(job) {
+  if (job.payload && job.payload.length > 0) {
+    var payload = JSON.parse(job.payload.toString());
+    if (payload.syncId && payload.syncId.length == 24) {
+      var options = {
+        syncId: utils.app.ObjectID(payload.syncId),
+        isMaster: true
+      }
+      node.prepareSync(options, function(err, result) {
+        if (err) {
+          job.workComplete(JSON.stringify({result: false, reason: err.message}));
+        } else {
+          job.workComplete(JSON.stringify({result: true, data: result}));
+        }
+      });
+      return;
+    }
+  }
+  job.workComplete(JSON.stringify({result: false, reason: "no payload"}));
+});
+
 
 connect(connected);
