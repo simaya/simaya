dd37e894bf32b38ec0541e19fff2cf1ba7140aaf
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-01-19 23:49:57 +0800

    migrate script for user,jobtitle,organization

--- tools/migrate-simaya-l.js
+++ tools/migrate-simaya-l.js
@@ -17,33 +17,97 @@ var set = function(name, cb) {
   }, { multi: true }, cb)
 }
 
-var user = app.db("user")
+var user = app.db("user");
+var jobTitle = app.db("jobTitle");
+var organization = app.db("organization");
+
 var spinner = ["/", "-", "|", "\\"];
 var saved = 0;
-var mod = function(index, data) {
+var markUser = function(index, data) {
   if (index == data.length) {
-    console.log("Saved: ", saved, "of total", data.length);
+    console.log("Users -> saved: ", saved, "of total", data.length);
+    markJobTitles();
+  } else {
+    user.findOne({_id: data[index]._id}, function(e, item) {
+      process.stdout.write(spinner[(index % 4)] + " -> " + index + "/" + data.length + "\r");
+      var save = false;
+      item.origin = settings.simaya.installationId;
+      if (item && item.username != "admin") {
+        item.username = "u"+settings.simaya.installationId+":"+item.username;
+      } 
+      save = true;
+      if (save) {
+        saved ++;
+        user.save(item, function() {
+          markUser(index + 1, data);
+        });
+      } else {
+        markUser(index + 1, data);
+      }
+    });
+  }
+}
+var markJobTitle = function(index, data) {
+  if (index == data.length) {
+    console.log("jobTitle -> saved: ", saved, "of total", data.length);
+    markOrganizations();
+  } else {
+    jobTitle.findOne({_id: data[index]._id}, function(e, item) {
+      process.stdout.write(spinner[(index % 4)] + " -> " + index + "/" + data.length + "\r");
+      var save = false;
+      item.origin = settings.simaya.installationId;
+      save = true;
+      if (save) {
+        saved ++;
+        jobTitle.save(item, function() {
+          markJobTitle(index + 1, data);
+        });
+      } else {
+        markJobTitle(index + 1, data);
+      }
+    });
+  }
+}
+var markOrganization = function(index, data) {
+  if (index == data.length) {
+    console.log("organization -> saved: ", saved, "of total", data.length);
     process.exit();
     return;
-  }
-  user.findOne({_id: data[index]._id}, function(e, item) {
-    process.stdout.write(spinner[(index % 4)] + " -> " + index + "/" + data.length + "\r");
-    var save = false;
-    if (item && item.username != "admin") {
-      item.username = "u"+settings.simaya.installationId+":"+item.username;
+  } else {
+    organization.findOne({_id: data[index]._id}, function(e, item) {
+      process.stdout.write(spinner[(index % 4)] + " -> " + index + "/" + data.length + "\r");
+      var save = false;
+      item.origin = settings.simaya.installationId;
       save = true;
-    } 
-    if (save) {
-      saved ++;
-      user.save(item, function() {
-        mod(index + 1, data);
-      });
-    } else {
-      mod(index + 1, data);
-    }
+      if (save) {
+        saved ++;
+        organization.save(item, function() {
+          markOrganization(index + 1, data);
+        });
+      } else {
+        markOrganization(index + 1, data);
+      }
+    });
+  }
+}
+var markUsers = function(){
+  saved = 0;
+  user.findArray({}, {_id:1,date:1}, function(e, c) {
+    markUser(0, c);
   });
 }
-
+var markJobTitles = function(){
+  saved = 0;
+  jobTitle.findArray({}, {_id:1,date:1}, function(e, c) {
+    markJobTitle(0, c);
+  });
+}
+var markOrganizations = function(){
+  saved = 0;
+  organization.findArray({}, {_id:1,date:1}, function(e, c) {
+    markOrganization(0, c);
+  });
+}
 console.log("Standing by...");
 settings.db.open(function(){
   set("letter", function(err, result) {
@@ -52,9 +116,7 @@ settings.db.open(function(){
       if (err) console.log(err);
       set("organization", function(err, result) {
         if (err) console.log(err);
-          user.findArray({}, {_id:1,date:1}, function(e, c) {
-            mod(0, c);
-          })
+        markUsers();
       });
     });
   });
